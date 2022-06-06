a8af406727044547c2c617721c0b284aeabc65ab
Ramindu Nanayakkara <44117883+raminduns@users.noreply.github.com>
2022-06-06 17:04:10 +0530

    Set to headless true

--- playwright.config.js
+++ playwright.config.js
@@ -46,7 +46,7 @@ const config = {
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://staging.unvrs.io',
-    headless:false,
+    headless:true,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     launchOptions: {
